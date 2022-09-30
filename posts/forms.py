ec12f3652d566154e32de028f6f176dddc48a601
Abdurahim1234567890 <dzhanbakkiev@mail.ru>
2022-09-30 17:53:47 +0600

    5 home_work

--- posts/forms.py
+++ posts/forms.py
@@ -23,3 +23,16 @@ class PostForm(forms.Form):
         choices=POST_TYPE_CHOICES
     )
 
+
+class Commentform(forms.Form):
+    author = forms.CharField(
+        label='Автор',
+        max_length=100,
+        min_length=5
+    )
+    text = forms.CharField(
+        widget=forms.Textarea(),
+        label="Описание",
+        max_length=100,
+        min_length=5
+    )
